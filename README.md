099407fee2dd600b1d04c9bf7cfe89ff9bf500f0
Davidoar15 <davidoar15@gmail.com>
2023-08-24 18:29:45 -0300

    add/Filter_by_Country_correctly

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Here are the Steps to follow:
 - [x] Provide the option to color rows.
 - [x] Allow the data to be sorted by country.
 - [x] Enable the ability to delete a row.
-- [] Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
+- [x] Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
 - [] Handle any potential errors that may occur. 
-- [] Implement a feature that allows the user to filter the data by country.
-- [] Avoid sorting users again thedata when the user is changing filter by country.
+- [x] Implement a feature that allows the user to filter the data by country.
+- [x] Avoid sorting users again the data when the user is changing filter by country.
 - [] Sort by clicking on the column header.
 - [] Provide a README.md file with instruccions on how to run the app.
--- TechTest-UsersTable/src/App.tsx
+++ TechTest-UsersTable/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useMemo } from 'react'
 import './App.css'
 import UsersList from './components/UsersList';
 import { User } from './types';
@@ -8,6 +8,7 @@ function App() {
   const [showColors, setShowColors] = useState(false);
   const [sortByCountry, setSortByCountry] = useState(false);
   const originalUsers = useRef<User[]>([]); //? Save a value that we want share between renders, but when change not return to render the component
+  const [filterCountry, setFilterCountry] = useState<string | null>(null);
 
   useEffect(() => {
     fetch('https://randomuser.me/api?results=100')
@@ -29,11 +30,21 @@ function App() {
     setSortByCountry((prevState) => !prevState) 
   };
 
-  const sortedUsers = sortByCountry 
-    ? users.toSorted((a, b) => {
-      return a.location.country.localeCompare(b.location.country);
-    }) 
-    : users;
+  const filteredUsers = useMemo(() => {
+    return filterCountry !== null && filterCountry.length > 0
+      ? users.filter((user) => {
+        return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
+      })
+      : users;
+  }, [users, filterCountry]);
+
+  const sortedUsers = useMemo(() => {
+    return sortByCountry 
+      ? filteredUsers.toSorted(
+        (a, b) => a.location.country.localeCompare(b.location.country)
+      )
+      : filteredUsers
+  }, [filteredUsers, sortByCountry]);
 
   // * One solution to sort users and back to original array
   /*const sortedUsers = sortByCountry 
@@ -45,11 +56,11 @@ function App() {
   const handleDelete = (uuid: string) => {
     const filteredUsers = users.filter((user) => user.login.uuid !== uuid);
     setUsers(filteredUsers);
-  }
+  };
 
   const handleReset = () => {
     setUsers(originalUsers.current)
-  }
+  };
 
   return (
     <div className='App'>
@@ -66,6 +77,10 @@ function App() {
         <button onClick={handleReset}>
           Reset List
         </button>
+
+        <input placeholder='Search by Country' onChange={(event) => {
+          setFilterCountry(event.target.value)
+        }}/>
       </header>
       <main>
         <UsersList users={sortedUsers} showColors={showColors} handleDelete={handleDelete}/>
