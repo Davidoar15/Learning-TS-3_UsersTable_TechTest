20d027da82bb25db9e9ac31682edfd2e9a84edab
Davidoar15 <davidoar15@gmail.com>
2023-08-24 17:07:12 -0300

    add/Sort_by_Country_correctly

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Here are the Steps to follow:
 
 - [x] Fetch 100 rows of data using the API.
 - [x] Display the data in a table format.
-- [] Provide the option to color rows.
-- [] Allow the data to be sorted by country.
+- [x] Provide the option to color rows.
+- [x] Allow the data to be sorted by country.
 - [] Enable the ability to delete a row.
 - [] Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
 - [] Handle any potential errors that may occur. 
--- TechTest-UsersTable/src/App.tsx
+++ TechTest-UsersTable/src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import UsersList from './components/UsersList';
+import { User } from './types';
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
+  const [sortByCountry, setSortByCountry] = useState(false);
 
   useEffect(() => {
     fetch('https://randomuser.me/api?results=100')
@@ -21,6 +23,23 @@ function App() {
     setShowColors(!showColors);
   };
 
+  const toggleSortByCountry = () => { 
+    setSortByCountry((prevState) => !prevState) 
+  };
+
+  const sortedUsers = sortByCountry 
+    ? users.toSorted((a, b) => {
+      return a.location.country.localeCompare(b.location.country);
+    }) 
+    : users;
+
+  // * One solution to sort users and back to original array
+  /*const sortedUsers = sortByCountry 
+    ? [...users].sort((a, b) => {
+      return a.location.country.localeCompare(b.location.country);
+    }) 
+    : users;*/
+
   return (
     <div className='App'>
       <h4>Tech Test</h4>
@@ -28,9 +47,13 @@ function App() {
         <button onClick={toggleColors}>
           Color Rows
         </button>
+
+        <button onClick={toggleSortByCountry}>
+          {sortByCountry ? 'Not Sort by Country' : 'Sort by Country'}
+        </button>
       </header>
       <main>
-        <UsersList users={users} showColors={showColors}/>
+        <UsersList users={sortedUsers} showColors={showColors}/>
       </main>
     </div>
   )
